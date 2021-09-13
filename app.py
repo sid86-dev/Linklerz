53db28ce44d3bae5500458f2656924214357cdc4
sid86-dev <sid86harth@gmail.com>
2021-09-13 16:35:15 +0530

    > bugfix

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ from delete_mailer import delete_email
 from itsdangerous import URLSafeTimedSerializer
 import string
 import random
-
+from sqlalchemy import create_engine
+e = create_engine(
+    "mysql://uh0cnoyrm6zjx9r0:VglVd8ErFy0srvWHyu82@bvcxwbbp2ahmmcsixu69-mysql.services.clever-cloud.com:3306/bvcxwbbp2ahmmcsixu69", pool_recycle=1800)
 app = Flask(__name__)
 app.secret_key = 'my-secret-key'
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://uh0cnoyrm6zjx9r0:VglVd8ErFy0srvWHyu82@bvcxwbbp2ahmmcsixu69-mysql.services.clever-cloud.com:3306/bvcxwbbp2ahmmcsixu69"
@@ -15,6 +17,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 s = URLSafeTimedSerializer('Linklerz.li')
+
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -33,17 +36,22 @@ def encrypt(password):
     hash = hashlib.sha256(password.encode()).hexdigest()
     return hash
 
-def entry(username_get,userpass_encrypt, useremail_get):
-    entry = Users(username=username_get, password=userpass_encrypt,  email=useremail_get, plan = 'free'
-    , confirmation = 'no', linktype="", linkurl=""
-     )
+
+def entry(username_get, userpass_encrypt, useremail_get):
+    entry = Users(username=username_get, password=userpass_encrypt,  email=useremail_get, plan='free', confirmation='no', linktype="", linkurl=""
+                  )
     db.session.add(entry)
     db.session.commit()
-    
+
 
 def get_linktype(linktype):
     list_lintype = linktype.split(">")
+    # print(list_lintype)
+    # for i in list_lintype:
+    #     if i == "":
+    #         list_lintype.remove(i)
     return list_lintype
+
 
 def gen_token(email):
     token = s.dumps(email, salt='email-confirm')
@@ -58,15 +66,20 @@ def gen_word():
         return rand_letters
 
 # index route
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 # inbuilt function which takes error as parameter
-@app.errorhandler(404) 
+
+
+@app.errorhandler(404)
 def not_found(e):
-# defining function
-  return render_template("404.html")
+    # defining function
+    return render_template("404.html")
+
 
 @app.route('/settings/<string:username>')
 def settings(username):
@@ -81,7 +94,7 @@ def settings(username):
 def home(username):
     try:
         credentials = Users.query.filter_by(username=username).first()
-        linktype = credentials.linktype 
+        linktype = credentials.linktype
         list_linktype = get_linktype(linktype)
 
         return render_template('home.html', list_linktype=list_linktype, credentials=credentials)
@@ -89,24 +102,26 @@ def home(username):
         return redirect('/login')
 
 # edit route
+
+
 @app.route('/edit')
 def edit():
     username = session['user']
     if ('user' in session and session['user'] == username):
         credentials = Users.query.filter_by(username=username).first()
-        linktype = credentials.linktype 
-        linkurl = credentials.linkurl 
+        linktype = credentials.linktype
+        linkurl = credentials.linkurl
         list_linktype = get_linktype(linktype)
         list_linkurl = get_linktype(linkurl)
-       
+
         linkdic = {}
         for key in list_linktype:
             for value in list_linkurl:
                 linkdic[key] = value
                 list_linkurl.remove(value)
                 break
-        
-        plan = credentials.plan 
+
+        plan = credentials.plan
         if plan == "free":
             num = 6 - len(list_linktype)
         else:
@@ -126,7 +141,9 @@ def profile(username):
         return redirect('/login')
 
 # saving data
-@app.route('/save', methods = ['GET', 'POST'])
+
+
+@app.route('/save', methods=['GET', 'POST'])
 def save():
     if request.method == "POST":
         username = session['user']
@@ -134,14 +151,16 @@ def save():
             credentials = Users.query.filter_by(username=username).first()
             linktype = credentials.linktype
             list_linktype = get_linktype(linktype)
+            print(list_linktype)
             # get old edited inklist
             oldlist_linktype = []
             oldlist_linkurl = []
-            for item in list_linktype: 
+            for item in list_linktype:
                 old_linktype = request.form.get(f'{item}type', '')
                 old_linkurl = request.form.get(item, '')
-                oldlist_linktype.append(old_linktype)
-                oldlist_linkurl.append(old_linkurl)
+                if old_linktype != '' and old_linkurl != '':
+                    oldlist_linktype.append(old_linktype)
+                    oldlist_linkurl.append(old_linkurl)
 
             # get new edited inklist
             new_linktype = request.form.get(f'new_linktype', '')
@@ -150,17 +169,19 @@ def save():
             if new_linktype != "" and new_linkurl != "":
                 oldlist_linktype.append(new_linktype)
                 oldlist_linkurl.append(new_linkurl)
-           
+
             x = ">".join(oldlist_linktype)
             y = ">".join(oldlist_linkurl)
 
             credentials.linktype = x
             credentials.linkurl = y
-         
+
             db.session.commit()
             return redirect(f'/home/{username}')
 
 # delete route
+
+
 @app.route('/delete/<link_name>')
 def delete(link_name):
     username = session['user']
@@ -172,20 +193,22 @@ def delete(link_name):
         list_linkurl = get_linktype(linkurl)
         # get index and delete with the index
         index = list_linktype.index(link_name)
-        list_linktype.remove(link_name) 
-        list_linkurl.remove(list_linkurl[index]) 
+        list_linktype.remove(link_name)
+        list_linkurl.remove(list_linkurl[index])
 
         x = ">".join(list_linktype)
         y = ">".join(list_linkurl)
 
         credentials.linktype = x
-        credentials.linkurl = y   
+        credentials.linkurl = y
 
         db.session.commit()
         return redirect('/edit')
     return redirect('/edit')
-            
+
 # login route
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     try:
@@ -199,7 +222,8 @@ def login():
             userpass_get = request.form.get('password').lower()
             userpass_encrypt = encrypt(userpass_get)
             try:
-                credentials = Users.query.filter_by(password=userpass_encrypt).first()
+                credentials = Users.query.filter_by(
+                    password=userpass_encrypt).first()
                 if credentials.username == username_get and credentials.password == userpass_encrypt:
                     # set the session variable
                     session['user'] = credentials.username
@@ -213,6 +237,8 @@ def login():
         return render_template('login.html', login_fail=login_fail)
 
 # signup route
+
+
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     user_exist = "NO"
@@ -230,13 +256,14 @@ def signup():
             user_exist = "NO"
             if userpass_get == confirmpass_get:
                 try:
-                    credentials = Users.query.filter_by(email=useremail_get).first()
+                    credentials = Users.query.filter_by(
+                        email=useremail_get).first()
                     useremail = credentials.email
                     email_exist = "yes"
                     return render_template('signup.html', user_exist=user_exist, email_exist=email_exist)
                 except:
                     userpass_encrypt = encrypt(userpass_get)
-                    entry(username_get,userpass_encrypt,useremail_get)
+                    entry(username_get, userpass_encrypt, useremail_get)
                     session['user'] = username_get
                     return redirect(f'/sendconfirm/{useremail_get}')
             else:
@@ -245,18 +272,19 @@ def signup():
     return render_template('signup.html', user_exist=user_exist)
 
 
-# email send    
+# email send
 @app.route('/sendconfirm/<string:email>')
 def emailconfirm(email):
     try:
         username = session['user']
         token = gen_token(email)
         final_token = f"https://linklerz.cleverapps.io/confirm/{token}"
-      
+
         send_email(email, username, final_token)
         return render_template('confirm.html', email_address=email)
     except:
         return redirect('/error')
+
 
 @app.route('/confirm/<string:token>')
 def confirm_email(token):
@@ -268,34 +296,39 @@ def confirm_email(token):
     return render_template('confirm_email.html', credentials=credentials)
 
 # delete account
+
+
 @app.route('/deletelog/<string:username>')
 def delete_account(username):
     # try:
     if ('user' in session and session['user'] == username):
-        keyword = gen_word() 
+        keyword = gen_word()
         word = f"{username}{keyword}"
         return render_template('delete.html', username=username, word=word)
     # except:
         # return redirect('/login')
 
-@app.route('/deletecheck/<string:username>/<string:word>', methods = ['GET', 'POST'])
-def delete_check(username,word):
-    if request.method == "POST":
-            inputtext = request.form.get('inputtext')
-            if inputtext != word:
-                return redirect(f'/deletelog/{username}')
-            else:
-                credentials = Users.query.filter_by(username=username).first()
-                email = credentials.email
-                # delete data
-                db.session.delete(credentials)
-                db.session.commit()
-                # send email
-                delete_email(email,username)
 
-                return redirect('/logout')
+@app.route('/deletecheck/<string:username>/<string:word>', methods=['GET', 'POST'])
+def delete_check(username, word):
+    if request.method == "POST":
+        inputtext = request.form.get('inputtext')
+        if inputtext != word:
+            return redirect(f'/deletelog/{username}')
+        else:
+            credentials = Users.query.filter_by(username=username).first()
+            email = credentials.email
+            # delete data
+            db.session.delete(credentials)
+            db.session.commit()
+            # send email
+            delete_email(email, username)
+
+            return redirect('/logout')
     return redirect('/login')
 # Logging out
+
+
 @app.route('/logout')
 def logout():
     try:
@@ -310,18 +343,20 @@ def logout():
     '/li.<string:username>')
 def link(username):
     credentials = Users.query.filter_by(username=username).first()
-    linktype = credentials.linktype 
-    linkurl = credentials.linkurl 
+    linktype = credentials.linktype
+    linkurl = credentials.linkurl
     list_linktype = get_linktype(linktype)
     list_linkurl = get_linktype(linkurl)
-    
+
     linkdic = {}
     for key in list_linktype:
         for value in list_linkurl:
             linkdic[key] = value
             list_linkurl.remove(value)
             break
+    print(linkdic)
     return render_template('dark_theme.html', credentials=credentials, linkdic=linkdic)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
