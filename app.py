2b642d06f436e047ce77b2aab5a8dc586acfad1b
sid86-dev <sid86harth@gmail.com>
2021-11-07 01:45:51 +0530

    google auth alpha

--- app.py
+++ app.py
@@ -461,8 +461,8 @@ def recovery():
     return render_template('recovery.html', credentials=fixed_credentials, arg=arg)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
-
 # if __name__ == "__main__":
-#     app.run(debug=True)
+#     app.run(host="0.0.0.0")
+
+if __name__ == "__main__":
+    app.run(debug=True)
