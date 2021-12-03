22eb0a871eb880943c723a65a742294c76b8b15d
Siddhartha Roy <sid86harth@gmail.com>
2021-12-03 18:42:37 +0530

    back

--- app/db.py
+++ app/db.py
@@ -14,3 +14,4 @@ class Users(db.Model):
     theme = db.Column(db.String(50), nullable=False)
     auth = db.Column(db.String(10), nullable=False)
     phone = db.Column(db.String(20), nullable=False)
+
